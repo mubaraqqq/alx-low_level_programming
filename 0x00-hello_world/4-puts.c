e96722b8bb4d676095a22b79c52d80abbe8bfc48
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-11 23:03:55 +0100

    add function documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string - Programming is like ...
+ *
+ */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-
+    puts("\"Programming is like building a multilingual puzzle\n");
     return (0);
 }
