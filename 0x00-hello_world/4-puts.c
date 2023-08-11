339d6f07bf77e39c60e10cb087b32886ca13f9a4
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-11 23:09:01 +0100

    fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - prints a string - Programming is like ...
  *
+ * Return: Function returns an integer
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
