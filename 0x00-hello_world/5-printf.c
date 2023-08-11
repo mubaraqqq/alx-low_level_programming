b350a916a80774393299513885584a15030edc07
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-11 23:26:27 +0100

    fix trailing whitespace

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - print string
- * 
+ *
  * Return: return integer
 */
 
-int main(void) 
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
