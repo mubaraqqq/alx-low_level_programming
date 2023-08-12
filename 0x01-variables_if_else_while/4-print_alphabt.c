f20a4209ba3b88314339a2477dd5c57bf0682676
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-12 01:15:15 +0100

    rename file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: return 0
+*/
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'q' || c == 'e')
+continue;
+else putchar(c);
+}
+
+
+putchar('\n');
+
+return (0);
+}
