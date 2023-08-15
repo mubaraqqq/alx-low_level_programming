b79c827a31d7e3ea3e66d0ca5bb23061c483e906
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-15 23:22:35 +0100

    fix documentation

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#include "ctype.h"
 
 /**
  * _islower - checks lowercase
- * @param c - integer parameter
+ * @c - integer parameter
  * Return: 1 if true and 0 ptherwise
 */
 
 int _islower(int c)
 {
-if (c == tolower(c))
+if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
