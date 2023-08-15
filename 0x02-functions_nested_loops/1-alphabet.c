f915ae0d405e5c22d672c1fd3cdfee21dad51083
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-15 22:42:10 +0100

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - using putchar to print alphabet
+ *
+ * Return: return integer 0
+*/
+
+int main(void)
+{
+char i;
+for (i = 'a'; i < 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
