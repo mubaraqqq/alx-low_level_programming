ef35e9e4fe539a0f78d341a1592869886686671a
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-15 23:39:57 +0100

    last digit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,9 +9,19 @@
 int print_sign(int n)
 {
 if (n > 0)
-return ('+');
+{
+_putchar('+');
+return (1);
+}
 else if (n == 0)
+{
+_putchar(0);
 return (0);
+}
 else
-return ('-');
+{
+_putchar('-');
+return (-1);
+}
+
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - absolute integer
+ * @n: integer parameter
+ * Return: 1 if true and 0 ptherwise
+*/
+
+int print_last_digit(int n)
+{
+
+/* your code goes there */
+int last_digit = n % 10;
+return (last_digit);
+}
