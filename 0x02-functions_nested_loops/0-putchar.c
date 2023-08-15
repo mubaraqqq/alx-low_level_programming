79162adc793fb0b9b7f8926f0028008c71c453c7
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-15 22:37:43 +0100

    fix betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 /**
  * main - using putchar to print
  *
- * Return: return integer 0  
+ * Return: return integer 0
 */
 
-int main (void)
+int main(void)
 {
 char str[] = "_putchar";
 int i;
-for (i = 0; i < 10; i ++)
+for (i = 0; i < 10; i++)
 {
 _putchar(str[i]);
 }
