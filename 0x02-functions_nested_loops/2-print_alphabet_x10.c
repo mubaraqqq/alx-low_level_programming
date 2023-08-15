29a81a37271091a935fb539dc085f5869b737372
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-15 23:00:48 +0100

    print alphbet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,17 @@
  * print_alphabet - using putchar to print alphabet
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
+int  count;
+
+for (count = 0; count < 10; count++)
+{
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
+}
 }
