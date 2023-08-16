f08ce8d3e83e9ad1e813e256c49f48d28f9c35b3
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-16 14:22:11 +0100

    fix if greater than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,32 @@
 void print_to_98(int y)
 {
 int n;
+
+if (y > 98)
+{
+for (n = y; n > 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}    
+}
+
+else
+{
 for (n = y; n < 99; n++)
 {
 printf("%d", n);
 if (n != 98)
 {
 putchar(',');
-putchar(' ');}
+putchar(' ');
 }
+}
+}
+
 putchar('\n');
 }
