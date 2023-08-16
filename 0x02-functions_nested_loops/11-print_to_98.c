fba22397d8d72e0ca6ca9ec62edc660bea38622a
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-16 14:13:15 +0100

    fix print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,19 @@ void print_to_98(int y)
 int n;
 for (n = y; n < 99; n++)
 {
-if (n != 0)
-{
-_putchar(' ');
-}
 if (n <= 9)
 {
 _putchar(n + '0');
 }
 else if (n > 9)
 {
-_putchar(' ');
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 }
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');}
 }
 _putchar('\n');
 }
