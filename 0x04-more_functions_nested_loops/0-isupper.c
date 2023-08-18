7d76e0b776ec0c4422a3d4c9e06e04b85007ab6f
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-18 07:34:19 +0100

    fix functin name

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - check if a character is in upper case
+ * _isuper - check if a character is in upper case
  * @c: character parameter to check for
  * Return: 1 if uppercase, 0 otherwise
 */
 
-int _isupper(int c)
+int _isuper(int c)
 {
 if ('A' <= c <= 'Z')
 return (1);
