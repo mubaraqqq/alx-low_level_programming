2838977eedba373ea460ff37060b8d213e45b3d7
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-18 08:12:33 +0100

    multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipies two digits
+ * @a: first operand
+ * @b: second operand
+ * Return: 1 if digit, 0 otherwise
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
