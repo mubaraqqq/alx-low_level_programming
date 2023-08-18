49c810c3d2edffae408f7feef214e78d94a5da13
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-18 10:38:05 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - print fizz buzz
+*/
+
+void main(void)
+{
+int n;
+
+for (n = 1; n < 101; n++)
+{
+if (n % 3 == 0 && n % 5 != 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else if (n % 5 == 0 && n % 3 != 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else
+{
+printf("%d", n);
+printf(" ");
+}
+}
+}
