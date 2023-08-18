726acd39490970bba31690b2bef68079ed7e1bac
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-18 08:36:38 +0100

    print more numbers + edit to other functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers;.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14, ten times
+*/
+
+void more_numbers(void)
+{
+int i;
+int count;
+
+for (count = 0; count < 9; count++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+
+}
