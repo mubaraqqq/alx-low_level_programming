4038a67c927a0a360050d89998f5fe8cdbe588be
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-18 07:35:37 +0100

    change file name

--- 0x04-more_functions_nested_loops/0-isuper.c
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isuper - check if a character is in upper case
+ * _isupper - check if a character is in upper case
  * @c: character parameter to check for
  * Return: 1 if uppercase, 0 otherwise
 */
 
-int _isuper(int c)
+int _isupper(int c)
 {
 if ('A' <= c <= 'Z')
 return (1);
