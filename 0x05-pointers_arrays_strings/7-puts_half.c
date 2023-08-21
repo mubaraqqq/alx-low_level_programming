310e1f48d042d6979ab873cb58ff4f53dd6a1aa8
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-21 22:57:07 +0100

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - print string followed by new line
+ * @str: char parameter
+*/
+
+void puts_half(char *str)
+{
+int i = 0;
+int mid;
+
+while (str[i] != '\0')
+i++;
+
+if (i % 2 != 0)
+mid = (i - 1) / 2;
+else
+mid = i  / 2;
+
+while (mid < i)
+{
+_putchar(str[mid]);
+mid++;
+}
+_putchar('\n');
+}
