5a7a010a28b497f6e9fa2a7acdb7c14f91768187
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-21 22:00:12 +0100

    fix betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - updates value that pointer parameter points to to 98 
+ * reset_to_98 - updates value that pointer parameter points to to 98
  * @n: pointer to parameter
 */
 
