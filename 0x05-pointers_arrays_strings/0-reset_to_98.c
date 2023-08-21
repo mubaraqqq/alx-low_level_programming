c57db9fc7daab123e93c75ad82fed9695fd62a06
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-21 21:58:03 +0100

    reset pointer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates value that pointer parameter points to to 98 
+ * @n: pointer to parameter
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
