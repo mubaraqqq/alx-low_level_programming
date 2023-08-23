f0bd84edb922f5bff3e8d345bb743362026c9359
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-23 21:57:26 +0100

    feat: add leet function

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encode string using 1337
+ * @s: string to encode
+ * Return: pointer to string
+*/
+
+char *leet(char *s)
+{
+char *str = s;
+
+while (*s != '\0')
+{
+if (*s == 'a' || *s == 'A')
+*s = '4';
+else if (*s == 'e' || *s == 'E')
+*s = '3';
+else if (*s == 'o' || *s == 'O')
+*s = '0';
+else if (*s == 't' || *s == 'T')
+*s = '7';
+else if (*s == 'l' || *s == 'L')
+*s = '1';
+s++
+;
+}
+*s = '\0';
+return (str);
+}
