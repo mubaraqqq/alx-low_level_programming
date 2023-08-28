a62617fd35047e032c3b4283f60fe02db869e6a5
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-28 17:13:46 +0100

    feat strspn: create function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix of a string
+ * @s: string parameter
+ * @accept: string to check for
+ * Return: Integer of prefix length
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+int found;
+int j;
+char *ptr = accept;
+unsigned int count = 0;
+
+while (*s != '\0')
+{
+
+found = 0;
+while (accept[j] != '\0')
+{
+if (*s == accept[j])
+found = 1;
+
+j++;
+}
+
+j = 0;
+if (found == 1)
+count++;
+else
+break;
+
+s++;
+}
+
+return (count);
+
+}
