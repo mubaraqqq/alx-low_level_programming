1c9bd9e7e183cb0245639aaadacb9492903d4178
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-28 21:04:02 +0100

    feat set_string: create function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set pointer to a string to a value
+ * @s: string pointer to change
+ * @to: string to change it to
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
