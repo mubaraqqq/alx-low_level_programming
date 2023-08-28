bcab1012fbe3f738668565aedaeb0df788b37761
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-28 16:20:27 +0100

    fix memcpy: add pointer to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,6 +11,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+char *ptr = dest;
 
 for (i = 0; i <= n; i++)
 {
@@ -19,5 +20,5 @@ dest++;
 src++;
 }
 
-return (dest);
+return (ptr);
 }
