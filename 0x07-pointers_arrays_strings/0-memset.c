28d93f3f2df3d82b8feb54892c94a49986c82fa2
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-28 14:10:21 +0100

    feat memset: add function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - set a memory location with a single byte
+ * @s: char location/pointer
+ * @b: single char byte
+ * @n: integer representing number of times to set byte
+ * Return: pointer to character
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+int i;
+
+for (i = 0; i < n; i++)
+{
+*s = b;
+s++;
+}
+
+
+return (ptr);
+}
