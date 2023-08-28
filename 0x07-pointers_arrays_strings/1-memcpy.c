9e5d77612df0ac7117f36e6d40985b988686222e
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-28 16:01:14 +0100

    feat memcpy: create function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy n bytes from dest to src
+ * @dest: destination string
+ * @src: source string
+ * Return: return pointer to string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+*dest = *src;
+dest++;
+src++;
+}
+
+return (dest);
+}
