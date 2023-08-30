6aca57bedc688ef972cedc9a5fae742aa70778ad
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-30 22:32:51 +0100

    feat sqrt recursion: create function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return square root a number
+ * @n: integer parameter
+ * Return: integer square root
+*/
+
+int _sqrt_recursion(int n)
+{
+int mid, next;
+
+if (n < 0)
+return (-1);
+else if (n == 0 || n == 1)
+return (n);
+else
+{
+mid = n / 2;
+next = _sqrt_recursion(mid);
+if (next * next == n)
+return (next);
+else
+return _sqrt_recursion(mid + 1);
+}
+}
