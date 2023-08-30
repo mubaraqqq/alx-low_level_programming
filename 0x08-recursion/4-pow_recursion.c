235aeeaee4a1afea59dfc150611806bb655a5c94
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-30 09:44:29 +0100

    feat pow_recursion: create function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the power of a number to another number
+ * @x: integer parameter
+ * @y: integer parameter, power
+ * Return: power of x to y
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+return(x * _pow_recursion(x, y - 1));
+}
