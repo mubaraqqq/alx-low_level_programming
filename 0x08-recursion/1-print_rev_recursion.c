64e0fbfb74d1380251013ad521d635f369e468e0
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-08-30 09:25:40 +0100

    fix print_rev_recursion: print new line

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 /**
  * _print_rev_recursion - print a string in revese recursively
@@ -9,14 +10,11 @@ void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 return;
-else
-{
-s++;
-_print_rev_recursion(s);
+
+_print_rev_recursion(s + 1);
 if (*s == '\n')
 _putchar('\n');
 else
 _putchar(*s);
-}
 
 }
