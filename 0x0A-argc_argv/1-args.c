e92eb9dd21ffbecdd0276a944073701a40f29238
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-04 22:20:19 +0100

    feat args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of arguments 
+ * @argc: first
+ * @argv: second
+ * Return: integer
+*/
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n", argc - 1);
+    return (EXIT_SUCCESS);
+}
