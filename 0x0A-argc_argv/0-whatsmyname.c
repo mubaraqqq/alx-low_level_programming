d913a609ddbfff8d5ac08cbed424cd820547abc5
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-04 22:14:47 +0100

    fix whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - print program name
+ * @argc: length of argv array
+ * @argv: array of arguments
+ * Return: Integer
+*/
+
 int main(int argc, char *argv[])
 {
 (void)argc;
