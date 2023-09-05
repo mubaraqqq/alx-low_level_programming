8daf8e0be66a44b6b93cf77b779a20232f554b0b
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-05 22:36:46 +0100

    fit strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,16 +20,11 @@ end++;
 
 new_str = (char *)malloc(end - str + 1);
 
-  // Allocate memory for the new string
-
-  // Copy the string to the new memory, one character at a time
 for (int i = 0; i < end - str; i++)
 new_str[i] = str[i];
 
-  // Terminate the new string with a null character
 new_str[end - str] = '\0';
 
-  // Return the pointer to the new string
 free(new_str);
 return (new_str);
 }
