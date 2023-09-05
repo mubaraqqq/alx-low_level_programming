031436cae3ecf90fdc61985bb202e2ddcad24792
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-05 22:37:36 +0100

    fix strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,6 +11,7 @@ char *_strdup(char *str)
 {
 char *end = str;
 char *new_str;
+int i;
 
 if (str == NULL)
 return (NULL);
@@ -20,7 +21,7 @@ end++;
 
 new_str = (char *)malloc(end - str + 1);
 
-for (int i = 0; i < end - str; i++)
+for (i = 0; i < end - str; i++)
 new_str[i] = str[i];
 
 new_str[end - str] = '\0';
