fbf58247172c94882abb9510bb9c671522ea6f7a
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-07 23:57:13 +0100

    fix realloc: remove initialization

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -44,7 +44,7 @@ void *new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
 
-for (unsigned int i = 0; i < old_size; i++)
+for (i = 0; i < old_size; i++)
 ((char *)new_ptr)[i] = ((char *)ptr)[i];
 
 free(ptr);
