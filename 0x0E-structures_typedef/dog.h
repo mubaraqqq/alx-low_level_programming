4743c02e38e35933a4a828687f7c6d705c3f4927
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-11 18:46:37 +0100

    fix dog: add description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
