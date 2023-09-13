87b355d82c368b9f0e44b3b36d45c9ccf3ffe6cd
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-13 21:06:27 +0100

    fix print_name: add function prototype to header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef function_pointers_h
 #define function_pointers_h
 int _putchar(char);
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, int size, void (*action)(int));
 #endif
