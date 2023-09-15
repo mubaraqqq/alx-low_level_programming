6f67856da86336529864bcf7b762c602e2071123
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-09-15 21:52:35 +0100

    fix print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,12 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_numbers - print numbers
+ * @separator: separator string
+ * @n: number of arguments
+*/
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 int i;
